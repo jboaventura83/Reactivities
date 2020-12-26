9236058affbefc361c91d48b90f53caf01170ae4
João Boaventura <jboaventura83@gmail.com>
2020-12-26 18:53:51 -0300

    end of de section 5

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { Menu, Container, Button } from "semantic-ui-react";
 
-export const NavBar = () => {
+interface IProps {
+  openCreateForm: () => void;
+}
+
+export const NavBar: React.FC<IProps> = ({ openCreateForm }) => {
   return (
     <Menu fixed="top" inverted>
       <Container>
         <Menu.Item header>
-            <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
-            Reactivities
+          <img
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginRight: "10px" }}
+          />
+          Reactivities
         </Menu.Item>
         <Menu.Item name="Activities" />
         <Menu.Item>
-            <Button positive content='Create Activity' />
+          <Button onClick={openCreateForm} positive content="Create Activity" />
         </Menu.Item>
       </Container>
     </Menu>
   );
 };
-
-export default NavBar;
